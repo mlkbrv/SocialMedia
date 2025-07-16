e49d7533cd0dcb54e75ec3a1e27b41a10dfecb4b
E <emilakbarov857@gmail.com>
2025-07-17 03:06:18 +0400

    fixed logout and added base.html

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from .views import *
+from django.contrib.auth import views as auth_views
 urlpatterns = [
        path('login/', user_login, name='login'),
+       path('logout/', user_logout, name='logout'),
+       path('',index,name='index'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,30 @@
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
 from .forms import LoginForm
 
 def user_login(request):
-    form = LoginForm()
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect('/users/')
+            else:
+                return HttpResponse("invalid credentials")
+
+    else:
+        form = LoginForm()
     return render(request,'users/login.html',{'form':form})
+
+@login_required
+def index(request):
+    return render(request,'users/index.html')
+
+def user_logout(request):
+    logout(request)
+    return render(request, 'users/logout.html')
